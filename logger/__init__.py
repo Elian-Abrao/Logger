dfa21fce25c058fda844a3d27ed128a85507a62d
Elian Abrao <elian.abrao@treeinova.com.br>
2025-06-19 20:31:28 -0300

    Adiciona exportação do método start_logger no módulo __init__.py

--- logger/__init__.py
+++ logger/__init__.py
@@ -3,4 +3,6 @@
 Use `start_logger` to create a configured logger instance.
 """
 
-from .core.logger_core import start_logger
+from .core.logger_core import start_logger as start_logger
+
+__all__ = ["start_logger"]
