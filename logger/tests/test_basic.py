8a50645930058f57f5fa5f878cff9795ef4450c8
Elian Abrao <elian.abrao@treeinova.com.br>
2025-06-19 18:03:07 -0300

    Remove testes não utilizados e ajusta a estrutura do código no main.py

--- logger/tests/test_basic.py
+++ logger/tests/test_basic.py
@@ -1,11 +1,1 @@
-import os
-from logger import start_logger
-
-def test_start_logger(tmp_path):
-    logdir = tmp_path / "logs"
-    logger = start_logger("test", log_dir=str(logdir))
-    logger.start()
-    logger.info("ok")
-    logger.end()
-    assert os.path.exists(logger.log_path)
-    assert os.path.exists(logger.debug_log_path)
+"""test_basic.py - Arquivo para testar todas as funcionalidades disponiveis no logger"""
--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from logger import start_logger
 def main():
     logger = start_logger("Demo")
     logger.start()
+
     logger.info("Processo iniciado")
-    with logger.context("Etapa"):  # from context module via start_logger
-        with logger.timer("demo"):
-            for i in logger.progress(range(5), desc="Trabalhando"):
-                logger.success("testeee")
-                logger.sleep(0.2)
-    logger.capture_prints(True)
+
+    for i in logger.progress(range(5), desc="Trabalhando"):
+        logger.success("testeee")
+        logger.sleep(0.2)
+        logger.debug("testeee")
+
     print("Mensagem exemplo")
-    logger.capture_prints(False)
     logger.end()
 
 
